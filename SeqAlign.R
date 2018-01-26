7eb0be9b4936b4221633d34342b006de66b3e3c6
Pavan Nichani <psn1997@users.noreply.github.com>
2018-01-26 16:39:14 +0530

    Add files via upload

--- SeqAlign.R
+++ SeqAlign.R
@@ -19,3 +19,15 @@ pairalign<-pairwiseAlignment(pattern = seq2, subject = seq1)
 #Convert Alignment to FASTA and save
 pairalignString = BStringSet( c( toString( subject(pairalign) ), toString(pattern(pairalign))))
 writeXStringSet(pairalignString, "aligned.txt", format="FASTA")
+
+#Create a Dotplot using seqinr 
+coxgenes<-read.fasta(file = "cox1multi.fasta", seqtype="AA")
+cox1<-as.character(coxgenes[[1]])
+cox2<-as.character(coxgenes[[2]])
+
+#Simple Dotplot
+dotPlot(cox1, cox2, main = "Human vs Mouse Cox1 Dotplot")
+#Windowed Dotplot
+dotPlot(cox1, cox2, wsize = 3, wstep = 3, nmatch = 3, main = "Human vs Mouse Cox1 Dotplot\nwsize = 3, wstep = 3, nmatch = 3")
+#Dotplot of only the first 100 residues of the sequence
+dotPlot(cox1[1:100], cox2[1:100], wsize = 3, wstep = 3, nmatch = 3, main = "Human vs Mouse Cox1 first 100 AA Dotplot\nwsize = 3, wstep = 3, nmatch = 3")
